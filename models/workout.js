10b08bd1743ab33e408add4136ce025b654051a0
you know who <chorvickprocode@gmail.com>
2021-04-19 07:23:40 -0400

    fixed type server will not start

--- models/workout.js
+++ models/workout.js
@@ -39,7 +39,7 @@ const workoutSchema = new Schema(
                     type: Number,
                 },
                 distance: {
-                    type: SVGAnimatedNumber,
+                    type: Number,
                 },
 
             }
