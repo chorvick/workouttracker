ed771a65bf788ce52f005fefa5c47d32f09031bb
you know who <chorvickprocode@gmail.com>
2021-04-19 07:34:07 -0400

    changed to single quotes fixed html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,25 +1,25 @@
 /// state dependencies first
 
-const router = require("express").Router();
-const path = require("path)");
+const router = require('express').Router();
+const path = require('path');
 
 
 ///html routes necessary -- first route is default/index html
 
-router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 
 
 });
 
 
-router.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/stats.html"));
+router.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/stats.html'));
 });
 
 
-router.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/exercise.html"))
+router.get('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'))
 });
 
 module.exports = router;
