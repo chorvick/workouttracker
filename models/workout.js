85aafc297e1efd954e4672840c702c4d972ddb88
you know who <chorvickprocode@gmail.com>
2021-04-19 06:19:27 -0400

    workout model and exercises complete

---
+++ models/workout.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+const logger = require("morgan");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+
+    {
+        day: {
+            type: Date,
+            default: Date.now,
+        },
+
+
+        exercises: [
+            {
+                type: {
+                    type: String,
+                    trim: true,
+                    required: "Please enter exercise type",
+                },
+                name: {
+                    type: String,
+                    trim: true,
+                    required: "Please enter name of exercise",
+                },
+                duration: {
+                    type: Number,
+                    required: "Please enter minutes of exercise duration",
+                },
+                weight: {
+                    type: Number,
+
+                },
+                reps: {
+                    type: Number,
+                },
+                sets: {
+                    type: Number,
+                },
+                distance: {
+                    type: SVGAnimatedNumber,
+                },
+
+            }
+        ]
+    },
+
+
+
+
+
+
+
+
+
+
+)
+
+
+const Workout = mongoose.model("Workout", workoutSchema);
+module.exports = Workout;
