e2b9fe916d2444f7d83a08412228d423fc35d1e1
you know who <chorvickprocode@gmail.com>
2021-04-19 20:07:31 -0400

    with compression npm pack added

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 const path = require("path");
-
+const compression = require('compression')
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-
+app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(logger("dev"));
